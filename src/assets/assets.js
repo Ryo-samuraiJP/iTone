3725b1e7782faa14c016ed62b0c361a51e438760
Rio <onlytru6340@gmail.com>
2024-09-25 01:08:28 -0700

    Update assets.js file

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -72,111 +72,127 @@ export const assets = {
 export const albumsData = [
     {   
         id:0,
-        name: "Top 50 Global",
+        name: "Nine Track Mind",
         image: img8,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#2a4365"
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#2a4365"
     },
     {   
         id:1,
-        name: "Top 50 India",
+        name: "Viva La Vida or Death and All His Friends",
         image: img9,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#22543d"
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#22543d"
     },
     {   
         id:2,
-        name: "Trending India",
+        name: "Daniel Powter",
         image: img10,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#742a2a"
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#742a2a"
     },
     {   
         id:3,
-        name: "Trending Global",
+        name: "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?",
         image: img16,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#44337a"
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#44337a"
     },
     {   
         id:4,
-        name: "Mega Hits,",
+        name: "Sunroof - Single",
         image: img11,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#234e52"
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#234e52"
     },
     {   
         id:5,
-        name: "Happy Favorites",
+        name: "My Everything",
         image: img15,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#744210"
+        desc: "Your weekly update of the most played tracks",
+        bgColor: "#744210"
     }
 ]
 
 export const songsData = [
     {
         id:0,
-        name: "Song One",
+        name: "Addicted To You",
         image: img1,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"3:00"
+        file: song1,
+        desc: "Avici's album",
+        duration: "2:28"
     },
     {
         id:1,
-        name: "Song Two",
+        name: "Calway Girl",
         image: img2,
-        file:song2,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:20"
+        file: song2,
+        desc: "Ed Sheeran's album",
+        duration: "2:51"
     },
     {
         id:2,
-        name: "Song Three",
+        name: "When I Was Your Man",
         image: img3,
-        file:song3,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:32"
+        file: song3,
+        desc: "Bruno Mars's album",
+        duration: "3:34"
     },
     {
         id:3,
         name: "Song Four",
         image: img4,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:50"
+        file: song1,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "2:50"
     },
     {
         id:4,
         name: "Song Five",
         image: img5,
-        file:song2,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"3:10"
+        file: song2,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "3:10"
     },
     {
         id:5,
         name: "Song Six",
-        image: img14    ,
-        file:song3,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:45"
+        image: img14,
+        file: song3,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "2:45"
     },
     {
         id:6,
         name: "Song Seven",
         image: img7,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:18"
+        file: song1,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "2:18"
     },
     {
         id:7,
         name: "Song Eight",
         image: img12,
-        file:song2,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:35"
-    }
+        file: song2,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "2:35"
+    },
+    {
+        id:8,
+        name: "Song Nine",
+        image: img6,
+        file: song3,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "3:10"
+    },
+    {
+        id:9,
+        name: "Song Ten",
+        image: img13,
+        file: song1,
+        desc: "Put a smile on your face with these happy tunes",
+        duration: "3:10"
+    },
 ]
