77273f87d2bc41bde189aca390926d11842210e1
Rio <onlytru6340@gmail.com>
2024-09-26 09:51:36 -0700

    Create PlayerContext component and add its logic to play/pause song in player

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Sidebar from './components/Sidebar';
 import MusicPlayer from './components/MusicPlayer';
 import Display from './components/Display';
+import { PlayerContext } from './context/PlayerContext';
 
 const App = () => {
+  const {audioRef, track} = useContext(PlayerContext);
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
         <Display />
       </div>
-        <MusicPlayer />
+      <MusicPlayer />
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   );
 }
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import { songsData } from '../assets/assets';
-import { IoIosShuffle, IoIosSkipBackward, IoIosSkipForward, IoIosPlay, IoIosInfinite } from "react-icons/io";
+import React, { useContext } from 'react';
+import { IoIosShuffle, IoIosSkipBackward, IoIosSkipForward, IoIosPlay, IoIosPause, IoIosInfinite } from "react-icons/io";
 import { AiOutlinePlaySquare } from "react-icons/ai";
 import { TbMicrophone2 } from "react-icons/tb";
 import { HiOutlineQueueList, HiOutlineSpeakerWave } from "react-icons/hi2";
 import { LuMonitorSpeaker } from "react-icons/lu";
 import { CgMiniPlayer } from "react-icons/cg";
 import { RiExpandDiagonalLine } from "react-icons/ri";
+import { PlayerContext } from '../context/PlayerContext';
 
 const MusicPlayer = () => {
+  const {track, seekBg, seekBar, playStatus, play, pause} = useContext(PlayerContext);
+
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
       <div className='hidden lg:flex items-center gap-4'>
-        <img className='w-12' src={songsData[0].image} alt='' />
+        <img className='w-12' src={track.image} alt='' />
         <div>
-          <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0, 12)}</p>
+          <p>{track.name}</p>
+          <p>{track.desc.slice(0, 12)}</p>
         </div>
       </div>
       <div className='flex flex-col items-center gap-1 m-auto'>
         <div className='flex gap-2'>
           <IoIosShuffle className='text-2xl cursor-pointer' />
           <IoIosSkipBackward className='text-2xl cursor-pointer' />
-          <IoIosPlay className='text-2xl cursor-pointer' />
+          {playStatus 
+            ? <IoIosPause onClick={pause} className='text-2xl cursor-pointer' />
+            : <IoIosPlay onClick={play} className='text-2xl cursor-pointer' />
+          }
           <IoIosSkipForward className='text-2xl cursor-pointer' />
           <IoIosInfinite className='text-2xl cursor-pointer' />
         </div>
         <div className='flex items-center gap-5'>
           <p>1:45</p>
-          <div className='w-[60vw] max-w-[500px] bg-gray-500 rounded-full cursor-pointer'>
-            <hr className='h-1 border-none w-0 bg-gray-200 rounded-full' />
+          <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-500 rounded-full cursor-pointer'>
+            <hr ref={seekBar} className='h-1 border-none w-0 bg-gray-200 rounded-full' />
           </div>
           <p>4:26</p>
         </div>
