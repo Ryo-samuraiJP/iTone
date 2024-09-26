eb5e8796d2b8e533a69533207b979cafe0284b4b
Rio <onlytru6340@gmail.com>
2024-09-25 21:28:20 -0700

    Create MusicPlayer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
+import MusicPlayer from './components/MusicPlayer';
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+      </div>
+      <div>
+        <MusicPlayer />
       </div>
     </div>
   );
---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { songsData } from '../assets/assets';
+import { IoIosShuffle, IoIosSkipBackward, IoIosSkipForward, IoIosPlay, IoIosInfinite } from "react-icons/io";
+import { HiOutlineQueueList, HiOutlineSpeakerWave } from "react-icons/hi2";
+import { LuMonitorSpeaker } from "react-icons/lu";
+import { CgMiniPlayer } from "react-icons/cg";
+import { RiExpandDiagonalLine } from "react-icons/ri";
+import { TbMicrophone2 } from "react-icons/tb";
+import { AiOutlinePlaySquare } from "react-icons/ai";
+
+const Player = () => {
+  return (
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+      <div className='hidden lg:flex items-center gap-4'>
+        <img className='w-12' src={songsData[0].image} alt='' />
+        <div>
+          <p>{songsData[0].name}</p>
+          <p>{songsData[0].desc.slice(0, 12)}</p>
+        </div>
+      </div>
+      <div className='flex flex-col items-center gap-1 m-auto'>
+        <div className='flex gap-2'>
+          <IoIosShuffle className='text-2xl cursor-pointer' />
+          <IoIosSkipBackward className='text-2xl cursor-pointer' />
+          <IoIosPlay className='text-2xl cursor-pointer' />
+          <IoIosSkipForward className='text-2xl cursor-pointer' />
+          <IoIosInfinite className='text-2xl cursor-pointer' />
+        </div>
+        <div className='flex items-center gap-5'>
+          <p>1:45</p>
+          <div className='w-[60vw] max-w-[500px] bg-gray-500 rounded-full cursor-pointer'>
+            <hr className='h-1 border-none w-0 bg-gray-200 rounded-full' />
+          </div>
+          <p>4:26</p>
+        </div>
+      </div>
+      <div className='text-lg hidden lg:flex items-center gap-1.5 opacity-75'>
+        <AiOutlinePlaySquare />
+        <TbMicrophone2 />
+        <HiOutlineQueueList />
+        <LuMonitorSpeaker />
+        <HiOutlineSpeakerWave />
+        <div className='w-20 bg-slate-50 h-1 rounded'></div>
+        <CgMiniPlayer />
+        <RiExpandDiagonalLine />
+      </div>
+    </div>
+  );
+}
+
+export default Player;
