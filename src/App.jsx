4f0e7c5246103beb447948593776b6c6df2b9786
Rio <onlytru6340@gmail.com>
2024-09-26 00:27:25 -0700

    Import Display component to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import MusicPlayer from './components/MusicPlayer';
+import Display from './components/Display';
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+        <Display />
       </div>
-      <div>
         <MusicPlayer />
-      </div>
     </div>
   );
 }
