e83ca654740dd2042c20127cca42919acae2e546
Rio <onlytru6340@gmail.com>
2024-09-30 02:14:30 -0700

    Use slice method to display the first 8 albums in the first line and rest of 3 albums in the second line

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react';
 import Navbar from './Navbar';
-import { albumsData, popSongs } from '../assets/assets';
+import { albumsData } from '../assets/assets';
 import AlbumItem from './AlbumItem';
-import SongItem from './SongItem';
 import { PlayerContext } from '../context/PlayerContext';
 
 const DisplayHome = () => {
@@ -14,7 +13,7 @@ const DisplayHome = () => {
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Select by genre</h1>
         <div className='flex overflow-auto'>
-          {albumsData.map((item, index) => (
+          {albumsData.slice(0, 8).map((item, index) => (
             <AlbumItem 
               key={index} 
               id={item.id} 
@@ -29,13 +28,14 @@ const DisplayHome = () => {
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
         <div className='flex overflow-auto'>
-          {popSongs.map((item, index) => (
-            <SongItem 
+          {albumsData.slice(8).map((item, index) => (
+            <AlbumItem 
               key={index}
               id={item.id}
               name={item.name}
               image={item.image}
               desc={item.desc}
+              playWithId={playWithId}
             />
           ))}
         </div>
