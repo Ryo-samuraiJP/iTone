47a7cdf7d5b8b126f7914dfeb9378d3e63c196be
Rio <onlytru6340@gmail.com>
2024-09-30 22:02:05 -0700

    Update home, back/next buttons and create search bar in the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import { IoIosClose, IoIosSearch } from "react-icons/io";
+import { AiFillHome } from 'react-icons/ai';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -9,29 +11,63 @@ const Navbar = () => {
     <>
       <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items-center gap-2'>
-          <div onClick={() => navigate(-1)} className='bg-gray-700 p-2 rounded-2xl cursor-pointer hover:bg-white group'>
-            <span className='group-hover:text-black flex'>
-              <MdArrowBackIos className='inline-block'/>
-              <p className='inline-block text-xs'>back</p>
-            </span>
+          <button className='bg-[#242424] hover:bg-[#3e3e3e] w-10 h-10 rounded-full flex items-center justify-center group transition-all duration-300'>
+            <AiFillHome onClick={() => navigate('/')} className='text-2xl text-gray-400 group-hover:text-white transition-all duration-300 cursor-pointer'/>
+          </button>
+          <div 
+            onClick={() => navigate(-1)} 
+            className='p-2 rounded-full cursor-pointer group bg-[#242424] hover:bg-[#3e3e3e] transition-all duration-300'
+          >
+            <button className='text-gray-400 group-hover:text-white flex transition-all duration-300'>
+              <FaArrowLeft />
+            </button>
           </div>
-          <div onClick={() => navigate(1)} className='bg-gray-700 p-2 rounded-2xl cursor-pointer hover:bg-white group'>
-            <span className='group-hover:text-black flex'>
-              <p className='inline-block text-xs'>next&nbsp;</p>
-              <MdArrowForwardIos className='inline-block'/>
-            </span>
+          <div 
+            onClick={() => navigate(1)}
+            className='p-2 rounded-full cursor-pointer group bg-[#242424] hover:bg-[#3e3e3e] transition-all duration-300'
+          >
+            <button className='text-gray-400 group-hover:text-white flex transition-all duration-300'>
+              <FaArrowRight />
+            </button>
+          </div>
+          <div className='group'>
+            <form className='form relative'>
+              <span className='absolute left-2 -translate-y-1/2 top-1/2 p-1'>
+                <IoIosSearch className='text-2xl text-gray-400 group-hover:text-white transition-all duration-300'/>
+              </span>
+              <input
+                className='rounded-full px-10 py-1.5 border-2 border-transparent focus:outline-none bg-[#242424] hover:bg-[#3e3e3e] focus:border-blue-500 placeholder-gray-400 transition-all duration-300 cursor-pointer' 
+                placeholder='Search...'
+                type='text'
+              />
+              <button className='absolute right-3 -translate-y-1/2 top-1/2 p-1' type='reset'>
+                <IoIosClose className='text-2xl text-gray-400 hover:text-white'/>
+              </button>
+            </form>
           </div>
         </div>
-        <div className='flex items-center gap-4'>
-          <p className='bg-black text-gray-300 font-bold text-[15px] py-1 px-3 rounded-2xl cursor-pointer'>Sign up</p>
-          <p className='bg-white text-black font-bold text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Log in</p>
-          {/* <p className='bg-green-400 text-black w-7 h-7 rounded-full flex items-center justify-center cursor-pointer'>R</p> */}
+        <div className='flex items-center gap-3'>
+          <button className='cursor-pointer font-bold'>
+            <p className='h-10 w-30 bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white text-[15px] py-2 px-3 rounded-full transition-all duration-300'>Sign up</p>
+          </button>
+          <button className='cursor-pointer font-bold'>
+            <p className='h-10 w-30 bg-white hover:bg-[#ffffffe6] text-black text-[15px] px-4 py-2 rounded-full transition-all duration-300 hidden md:block'>Log in</p>
+          </button>
+          {/* <span>
+            <p className='bg-blue-400 text-black w-7 h-7 rounded-full flex items-center justify-center cursor-pointer'>R</p>
+          </span> */}
         </div>
       </div>
       <div className='flex items-center gap-2 mt-4'>
-        <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
-        <p className='bg-gray-700 px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
-        <p className='bg-gray-700 px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+        <button className='cursor-pointer font-medium'>
+          <p className='bg-white hover:bg-[#ffffffe6] text-black px-4 py-1 rounded-full transition-all duration-300'>All</p>
+        </button>
+        <button className='cursor-pointer font-medium'>
+          <p className='bg-[#242424] hover:bg-[#3e3e3e] active:bg-white text-gray-400 hover:text-white active:text-black transition-all duration-300 px-4 py-1 rounded-full'>Music</p>
+        </button>
+        <button className='cursor-pointer font-medium'>
+          <p className='bg-[#242424] hover:bg-[#3e3e3e] active:bg-white text-gray-400 hover:text-white active:text-black transition-all duration-300 px-4 py-1 rounded-full'>Podcasts</p>
+        </button>
       </div>
     </>
   );
