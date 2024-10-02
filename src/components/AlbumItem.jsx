204dd1dd1806c1fab401041811d7ad14ea615654
Rio <onlytru6340@gmail.com>
2024-10-02 07:17:49 -0700

    Make the AlbumItem component responsive and apply the configured hover-animation for the PlayButton

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -11,7 +11,7 @@ const AlbumItem = ({ id, image, name, desc }) => {
   return (
     <div 
       onClick={() => navigate(`/album/${id}`)} 
-      className='relative min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#51505045]' 
+      className='relative min-w-[250px] p-2 px-3 rounded cursor-pointer hover:bg-[#51505045] transition-all duration-300 sm:min-w-[200px]' 
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)} 
     >
@@ -19,7 +19,7 @@ const AlbumItem = ({ id, image, name, desc }) => {
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
       {isHovered && (
-        <div className='absolute bottom-3 right-3 mb-24 mr-3'>
+        <div className='absolute bottom-3 right-3 mb-24 mr-3 transition-all animate-slide-up'>
           <PlayButton albumId={name} onClick={() => playWithId(0, name)} />
         </div>
       )}
