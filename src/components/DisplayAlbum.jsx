cdc3fd6eb128172508c4f81927a88265bf6df6d3
Rio <onlytru6340@gmail.com>
2024-10-01 20:29:50 -0700

    Implement to display the MusicEqualizer at the song number only while the song is playing

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -4,11 +4,12 @@ import { useParams } from 'react-router-dom';
 import { albumPlaylists, alternativeSongs, assets, clubHot, edmSongs, globalHot, hiphopSongs, houseSongs, japanHot, jpopSongs, popSongs, rbSongs, rockSongs } from '../assets/assets';
 import { LuClock } from "react-icons/lu";
 import { PlayerContext } from '../context/PlayerContext';
+import MusicEqualizer from './MusicEqualizer';
 
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumPlaylists[id];
-  const { playWithId } = useContext(PlayerContext);
+  const { playWithId, track, playStatus } = useContext(PlayerContext);
 
   const getSongsByGenre = (genre) => {
     switch (genre) {
@@ -54,44 +55,48 @@ const DisplayAlbum = () => {
   const minutes = totalDurationInMinutes % 60;
     
   return (
-      <>
-        <Navbar />
-        <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
-          <img className='w-48 rounded' src={albumData.image} alt='' />
-          <div className='flex flex-col'>
-            <p>Playlist</p>
-            <h2 className='text-5xl font-bold mb-6 md:text-7xl'>{albumData.name}</h2>
-            <h4 className='mb-2'>{albumData.desc}</h4>
-            <p className='mb-1 items-center justify-between'>
-              <span>
-                <img className='inline-block w-5 mr-2' src={assets.logo} alt='' />
-                <b>iTone</b>
-              </span>
-              <span className='text-gray-400'>
-                ・{songsByGenre.length} songs, about {hours} hr {minutes} min
-              </span>
-            </p>
-          </div>
+    <>
+      <Navbar />
+      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+        <img className='w-48 rounded' src={albumData.image} alt='' />
+        <div className='flex flex-col'>
+          <p>Playlist</p>
+          <h2 className='text-5xl font-bold mb-6 md:text-7xl'>{albumData.name}</h2>
+          <h4 className='mb-2'>{albumData.desc}</h4>
+          <p className='mb-1 items-center justify-between'>
+            <span>
+              <img className='inline-block w-5 mr-2' src={assets.logo} alt='' />
+              <b>iTone</b>
+            </span>
+            <span className='text-gray-400'>
+              <b>・</b>{songsByGenre.length} songs, about {hours} hr {minutes} min
+            </span>
+          </p>
         </div>
-        <div className='grid grid-cols-[2fr_2fr_0.5fr_0.4fr] sm:grid-cols-[2fr_2fr_0.5fr_0.4fr] mt-10 mb-2 pl-2 text-[#a7a7a7]'>
-          <span className='items-center justify-between'>
-            <p className='mr-4 inline-block font-semibold'>#</p>
-            <p className='inline-block'>Title</p>
-          </span>
-          <p className=''>Album</p>
-          <p className='hidden sm:block'>Release</p>
-          <LuClock className='m-auto text-lg' />
-        </div>
-        <hr className='border-1 border-slate-700 mb-3'/>
-        {
-          songsByGenre.map((item, index) => (
+      </div>
+      <div className='grid grid-cols-[2fr_2fr_0.5fr_0.4fr] sm:grid-cols-[2fr_2fr_0.5fr_0.4fr] mt-10 mb-2 pl-2 text-[#a7a7a7]'>
+        <span className='items-center justify-between'>
+          <p className='mr-4 inline-block font-semibold'>#</p>
+          <p className='inline-block'>Title</p>
+        </span>
+        <p className=''>Album</p>
+        <p className='hidden sm:block'>Release</p>
+        <LuClock className='m-auto text-lg' />
+      </div>
+      <hr className='border-1 border-gray-600 mb-3'/>
+      {
+        songsByGenre.map((item, index) => (
           <div 
             onClick={() => playWithId(index, albumData.name)} // Pass the correct index
             key={index}
             className='grid grid-cols-[2fr_2fr_0.5fr_0.3fr] sm:grid-cols-[2fr_2fr_0.5fr_0.3fr] gap-2 p-2 itemes-center text-[#a7a7a7] hover:bg-[#51505045] cursor-pointer'
           >
             <div className='text-white flex items-center'>
-              <b className='mr-4 text-[#a7a7a7] font-normal'>{index + 1}</b>
+              {track.id === item.id && playStatus ? (
+                <MusicEqualizer />
+              ) : (
+                <b className='mr-4 text-[#a7a7a7] font-normal'>{index + 1}</b>
+              )}
               <img className='inline w-10 mr-5 rounded' src={item.image} alt='' />
               <span className='inline-block'>
                 <span className=''>{item.name}</span>
@@ -110,7 +115,7 @@ const DisplayAlbum = () => {
           </div>
         ))
       }
-    </>
+    </>  
   );
 }
 
