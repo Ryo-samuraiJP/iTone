40e154fa0eaf2f3eb545bb27b6e499ce55344b58
Rio <onlytru6340@gmail.com>
2025-02-24 20:59:22 -0800

    Add comments in Display component, improving readability and maintainability

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,34 +1,40 @@
-import React, { useEffect, useRef } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import DisplayHome from './DisplayHome';
-import DisplayAlbum from './DisplayAlbum';
-import { albumPlaylists } from '../assets/assets';
-import Footer from './Footer';
+import React, { useEffect, useRef } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import DisplayHome from "./DisplayHome";
+import DisplayAlbum from "./DisplayAlbum";
+import { albumPlaylists } from "../assets/assets";
+import Footer from "./Footer";
 
+// Display component that displays the DisplayHome and DisplayAlbum components based on the current route
 const Display = () => {
   const displayRef = useRef();
   const location = useLocation();
-  const isAlbum = location.pathname.includes('album');
-  const albumId = isAlbum ? location.pathname.match(/album\/(\d+)/)[1] : '';
-  const bgColor = albumPlaylists[Number(albumId)].bgColor;
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.match(/album\/(\d+)/)[1] : ""; // Get the albumId from the URL
+  const bgColor = albumPlaylists[Number(albumId)].bgColor; // Get the bgColor based on the albumId
 
   useEffect(() => {
     if (isAlbum) {
-      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`
+      // Change the background color of the display based on the bgColor of the album
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
     } else {
-      displayRef.current.style.background = `#121212`
+      // Change the background color of the display to # 121212
+      displayRef.current.style.background = `#121212`;
     }
   });
 
   return (
-    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+    <div
+      ref={displayRef}
+      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+    >
       <Routes>
-        <Route path='/' element={<DisplayHome />} />
-        <Route path='/album/:id' element={<DisplayAlbum />} />
+        <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default Display;
