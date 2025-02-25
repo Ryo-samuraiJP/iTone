ae48963469c70690c9f8f18cc5b1418ff542115e
Rio <onlytru6340@gmail.com>
2025-02-24 20:56:55 -0800

    Add comments in Navbar and DisplayAlbum components, improving readability and maintainability

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,26 @@
-import React, { useContext } from 'react';
-import Navbar from './Navbar';
-import { useParams } from 'react-router-dom';
-import { albumPlaylists, alternativeSongs, assets, clubHot, edmSongs, globalHot, hiphopSongs, houseSongs, japanHot, jpopSongs, popSongs, rbSongs, rockSongs } from '../assets/assets';
+import React, { useContext } from "react";
+import Navbar from "./Navbar";
+import { useParams } from "react-router-dom";
+import {
+  albumPlaylists,
+  alternativeSongs,
+  assets,
+  clubHot,
+  edmSongs,
+  globalHot,
+  hiphopSongs,
+  houseSongs,
+  japanHot,
+  jpopSongs,
+  popSongs,
+  rbSongs,
+  rockSongs,
+} from "../assets/assets";
 import { LuClock } from "react-icons/lu";
-import { PlayerContext } from '../context/PlayerContext';
-import MusicEqualizer from './MusicEqualizer';
+import { PlayerContext } from "../context/PlayerContext";
+import MusicEqualizer from "./MusicEqualizer";
 
+// DisplayAlbum component that displays the album details and songs based on the album id
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumPlaylists[id];
@@ -13,117 +28,134 @@ const DisplayAlbum = () => {
 
   const getSongsByGenre = (genre) => {
     switch (genre) {
-      case 'All EDM':
+      case "All EDM":
         return edmSongs;
-      case 'All Hip Hop / Rap':
+      case "All Hip Hop / Rap":
         return hiphopSongs;
-      case 'All R&B':
+      case "All R&B":
         return rbSongs;
-      case 'All House':
+      case "All House":
         return houseSongs;
-      case 'All Rock':
+      case "All Rock":
         return rockSongs;
-      case 'All J-Pop':
+      case "All J-Pop":
         return jpopSongs;
-      case 'All Alternative':
+      case "All Alternative":
         return alternativeSongs;
-      case 'Hot 30 Global':
+      case "Hot 30 Global":
         return globalHot;
-      case 'Hot 20 Night Club': 
+      case "Hot 20 Night Club":
         return clubHot;
-      case 'Hot 10 Japan':
+      case "Hot 10 Japan":
         return japanHot;
       default:
         return popSongs;
     }
   };
-  
+
   const songsByGenre = getSongsByGenre(albumData.name);
 
+  // Convert the duration from 'mm:ss' to seconds
   const convertToSeconds = (duration) => {
-    const [minutes, seconds] = duration.split(':').map(Number);
-    return (minutes * 60) + seconds;
+    const [minutes, seconds] = duration.split(":").map(Number);
+    return minutes * 60 + seconds;
   };
-  
+
+  // Calculate the total duration of the songs in the album
   const totalDurationInSeconds = songsByGenre.reduce((total, song) => {
     const durationInSeconds = convertToSeconds(song.duration);
     return total + durationInSeconds;
   }, 0);
-  
-  const totalDurationInMinutes = Math.floor(totalDurationInSeconds / 60);
-  const hours = Math.floor(totalDurationInMinutes / 60);
-  const minutes = totalDurationInMinutes % 60;
 
+  const totalDurationInMinutes = Math.floor(totalDurationInSeconds / 60); // Convert the total duration to minutes
+  const hours = Math.floor(totalDurationInMinutes / 60); // Get the total hours
+  const minutes = totalDurationInMinutes % 60; // Get the total minutes
+
+  // Truncate the text based on the screen width
   const truncateText = (text) => {
     if (window.innerWidth <= 640) {
-      return text.length > 25 ? text.slice(0, 25) + '...' : text;
+      // Truncate the text to 25 characters if the screen width is less than or equal to 640px
+      return text.length > 25 ? text.slice(0, 25) + "..." : text;
     }
     return text;
-  }
-    
+  };
+
   return (
     <>
       <Navbar />
-      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
-        <img className='w-48 rounded' src={albumData.image} alt='album image' />
-        <div className='flex flex-col'>
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img className="w-48 rounded" src={albumData.image} alt="album image" />
+        <div className="flex flex-col">
           <p>Playlist</p>
-          <h2 className='text-5xl font-bold mb-6 md:text-7xl'>{albumData.name}</h2>
-          <h4 className='mb-2'>{albumData.desc}</h4>
-          <p className='mb-1 items-center justify-between'>
+          <h2 className="text-5xl font-bold mb-6 md:text-7xl">
+            {albumData.name}
+          </h2>
+          <h4 className="mb-2">{albumData.desc}</h4>
+          <p className="mb-1 items-center justify-between">
             <span>
-              <img className='inline-block w-5 mr-2' src={assets.logo} alt='iTone logo' />
+              <img
+                className="inline-block w-5 mr-2"
+                src={assets.logo}
+                alt="iTone logo"
+              />
               <b>iTone</b>
             </span>
-            <span className='text-gray-400'>
-              <b>・</b>{songsByGenre.length} songs, about {hours} hr {minutes} min
+            <span className="text-gray-400">
+              <b>・</b>
+              {songsByGenre.length} songs, about {hours} hr {minutes} min
             </span>
           </p>
         </div>
       </div>
-      <div className='grid grid-cols-[1fr_4fr] sm:grid-cols-[2fr_2fr_0.5fr_0.3fr] gap-2 p-2 items-center mt-10 text-[#a7a7a7]'>
-        <span className='flex items-center'>
-          <p className='mx-2 inline-block font-semibold'>#</p>
-          <p className='inline-block ml-16'>Title</p>
+      <div className="grid grid-cols-[1fr_4fr] sm:grid-cols-[2fr_2fr_0.5fr_0.3fr] gap-2 p-2 items-center mt-10 text-[#a7a7a7]">
+        <span className="flex items-center">
+          <p className="mx-2 inline-block font-semibold">#</p>
+          <p className="inline-block ml-16">Title</p>
         </span>
-        <p className='hidden sm:block ml-24 lg:ml-16'>Album</p>
-        <p className='hidden sm:block md:mr-6'>Release</p>
-        <LuClock className='m-auto text-lg hidden sm:block' />
+        <p className="hidden sm:block ml-24 lg:ml-16">Album</p>
+        <p className="hidden sm:block md:mr-6">Release</p>
+        <LuClock className="m-auto text-lg hidden sm:block" />
       </div>
-      <hr className='border-1 border-gray-600 mb-3'/>
-      {
-        songsByGenre.map((item, index) => (
-          <div 
-            onClick={() => playWithId(index, albumData.name)} // Pass the correct index
-            key={index}
-            className='grid grid-cols-[1fr_4fr] sm:grid-cols-[2fr_2fr_0.5fr_0.3fr] gap-2 p-2 itemes-center text-[#a7a7a7] hover:bg-[#51505045] cursor-pointer'
-          >
-            <div className='text-white flex items-center'>
-              {track.id === item.id && playStatus ? (
-                <MusicEqualizer />
-              ) : (
-                <b className='mr-4 text-[#a7a7a7] font-normal w-14 md:w-7 xl:w-6 text-right'>{index + 1}</b>
-              )}
-              <img className='inline w-10 mr-5 rounded' src={item.image} alt='each track image' />
-              <span className='flex flex-col w-full'>
-                <span className='truncate'>{truncateText(item.name)}</span>
-                <span className='block text-sm text-[#a7a7a7] truncate'>{item.artist}</span>
+      <hr className="border-1 border-gray-600 mb-3" />
+      {songsByGenre.map((item, index) => (
+        <div
+          onClick={() => playWithId(index, albumData.name)} // Pass the correct index
+          key={index}
+          className="grid grid-cols-[1fr_4fr] sm:grid-cols-[2fr_2fr_0.5fr_0.3fr] gap-2 p-2 itemes-center text-[#a7a7a7] hover:bg-[#51505045] cursor-pointer"
+        >
+          <div className="text-white flex items-center">
+            {track.id === item.id && playStatus ? (
+              <MusicEqualizer />
+            ) : (
+              <b className="mr-4 text-[#a7a7a7] font-normal w-14 md:w-7 xl:w-6 text-right">
+                {index + 1}
+              </b>
+            )}
+            <img
+              className="inline w-10 mr-5 rounded"
+              src={item.image}
+              alt="each track image"
+            />
+            <span className="flex flex-col w-full">
+              <span className="truncate">{truncateText(item.name)}</span>
+              <span className="block text-sm text-[#a7a7a7] truncate">
+                {item.artist}
               </span>
-            </div>
-            <div className='hidden items-center sm:flex md:ml-24 lg:ml-16'>
-              <p className='text-[15px]'>{item.album}</p>
-            </div>
-            <div className='fhidden items-center sm:flex'>
-              <p className='text-[15px] hidden sm:block'>{item.release}</p>
-            </div>
-            <div className='hidden items-center sm:flex justify-center'>
-              <p className='text-[15px]'>{item.duration}</p>
-            </div>
+            </span>
           </div>
-        ))
-      }
-    </>  
+          <div className="hidden items-center sm:flex md:ml-24 lg:ml-16">
+            <p className="text-[15px]">{item.album}</p>
+          </div>
+          <div className="fhidden items-center sm:flex">
+            <p className="text-[15px] hidden sm:block">{item.release}</p>
+          </div>
+          <div className="hidden items-center sm:flex justify-center">
+            <p className="text-[15px]">{item.duration}</p>
+          </div>
+        </div>
+      ))}
+    </>
   );
-}
+};
 
 export default DisplayAlbum;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,80 +1,102 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { IoIosClose, IoIosSearch } from "react-icons/io";
-import { AiFillHome } from 'react-icons/ai';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
-import { assets } from '../assets/assets';
+import { AiFillHome } from "react-icons/ai";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { assets } from "../assets/assets";
 
+// Navbar component that displays the Home and Arrow icons, and Search bar
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <div className='w-full flex justify-between items-center font-semibold'>
-        <div className='flex items-center gap-2'>
-          <img onClick={() => navigate('/')} src={assets.logo} alt='iTone logo' className='w-10 lg:hidden'/>
-          <div className='hidden sm:flex items-center gap-2'>
-            <button className='bg-[#242424] hover:bg-[#3e3e3e] w-10 h-10 rounded-full flex items-center justify-center group transition-all duration-300'>
-              <AiFillHome onClick={() => navigate('/')} className='text-2xl text-gray-400 group-hover:text-white transition-all duration-300 cursor-pointer'/>
+      <div className="w-full flex justify-between items-center font-semibold">
+        <div className="flex items-center gap-2">
+          <img
+            onClick={() => navigate("/")}
+            src={assets.logo}
+            alt="iTone logo"
+            className="w-10 lg:hidden"
+          />
+          <div className="hidden sm:flex items-center gap-2">
+            <button className="bg-[#242424] hover:bg-[#3e3e3e] w-10 h-10 rounded-full flex items-center justify-center group transition-all duration-300">
+              <AiFillHome
+                onClick={() => navigate("/")}
+                className="text-2xl text-gray-400 group-hover:text-white transition-all duration-300 cursor-pointer"
+              />
             </button>
           </div>
-          <div 
-            onClick={() => navigate(-1)} 
-            className='hidden sm:flex p-2 rounded-full cursor-pointer group bg-[#242424] hover:bg-[#3e3e3e] transition-all duration-300'
+          <div
+            onClick={() => navigate(-1)}
+            className="hidden sm:flex p-2 rounded-full cursor-pointer group bg-[#242424] hover:bg-[#3e3e3e] transition-all duration-300"
           >
-            <button className='text-gray-400 group-hover:text-white flex transition-all duration-300'>
+            <button className="text-gray-400 group-hover:text-white flex transition-all duration-300">
               <FaArrowLeft />
             </button>
           </div>
-          <div 
+          <div
             onClick={() => navigate(1)}
-            className='hidden sm:flex p-2 rounded-full cursor-pointer group bg-[#242424] hover:bg-[#3e3e3e] transition-all duration-300'
+            className="hidden sm:flex p-2 rounded-full cursor-pointer group bg-[#242424] hover:bg-[#3e3e3e] transition-all duration-300"
           >
-            <button className='text-gray-400 group-hover:text-white flex transition-all duration-300'>
+            <button className="text-gray-400 group-hover:text-white flex transition-all duration-300">
               <FaArrowRight />
             </button>
           </div>
-          <div className='group'>
-            <form className='form relative'>
-              <span className='absolute left-2 -translate-y-1/2 top-1/2 p-1'>
-                <IoIosSearch className='text-2xl text-gray-400 group-hover:text-white transition-all duration-300'/>
+          <div className="group">
+            <form className="form relative">
+              <span className="absolute left-2 -translate-y-1/2 top-1/2 p-1">
+                <IoIosSearch className="text-2xl text-gray-400 group-hover:text-white transition-all duration-300" />
               </span>
               <input
-                className='rounded-full px-9 ml-1 py-1.5 border-2 border-transparent focus:outline-none bg-[#242424] hover:bg-[#3e3e3e] focus:border-blue-500 placeholder-gray-400 transition-all duration-300 cursor-pointer' 
-                placeholder='Search...'
-                type='text'
+                className="rounded-full px-9 ml-1 py-1.5 border-2 border-transparent focus:outline-none bg-[#242424] hover:bg-[#3e3e3e] focus:border-blue-500 placeholder-gray-400 transition-all duration-300 cursor-pointer"
+                placeholder="Search..."
+                type="text"
               />
-              <button className='absolute right-3 -translate-y-1/2 top-1/2 p-1' type='reset'>
-                <IoIosClose className='text-2xl text-gray-400 hover:text-white'/>
+              <button
+                className="absolute right-3 -translate-y-1/2 top-1/2 p-1"
+                type="reset"
+              >
+                <IoIosClose className="text-2xl text-gray-400 hover:text-white" />
               </button>
             </form>
           </div>
         </div>
-        <div className='flex items-center gap-3'>
-          <button className='cursor-pointer font-bold'>
-            <p className='h-10 w-30 bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white text-[15px] py-2 px-3 rounded-full transition-all duration-300 hidden md:block'>Sign up</p>
+        <div className="flex items-center gap-3">
+          <button className="cursor-pointer font-bold">
+            <p className="h-10 w-30 bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white text-[15px] py-2 px-3 rounded-full transition-all duration-300 hidden md:block">
+              Sign up
+            </p>
           </button>
-          <button className='cursor-pointer font-bold'>
-            <p className='h-10 w-30 bg-white hover:bg-[#ffffffe6] text-black text-[15px] px-4 py-2 rounded-full transition-all duration-300 hidden md:block'>Log in</p>
+          <button className="cursor-pointer font-bold">
+            <p className="h-10 w-30 bg-white hover:bg-[#ffffffe6] text-black text-[15px] px-4 py-2 rounded-full transition-all duration-300 hidden md:block">
+              Log in
+            </p>
           </button>
           {/* <span>
             <p className='bg-blue-400 text-black w-7 h-7 rounded-full flex items-center justify-center cursor-pointer'>R</p>
           </span> */}
         </div>
       </div>
-      <div className='flex items-center gap-2 mt-4'>
-        <button className='cursor-pointer font-medium'>
-          <p className='bg-white hover:bg-[#ffffffe6] text-black px-4 py-1 rounded-full transition-all duration-300'>All</p>
+      <div className="flex items-center gap-2 mt-4">
+        <button className="cursor-pointer font-medium">
+          <p className="bg-white hover:bg-[#ffffffe6] text-black px-4 py-1 rounded-full transition-all duration-300">
+            All
+          </p>
         </button>
-        <button className='cursor-pointer font-medium'>
-          <p className='bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white transition-all duration-300 px-4 py-1 rounded-full'>Music</p>
+        <button className="cursor-pointer font-medium">
+          <p className="bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white transition-all duration-300 px-4 py-1 rounded-full">
+            Music
+          </p>
         </button>
-        <button className='cursor-pointer font-medium'>
-          <p className='bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white transition-all duration-300 px-4 py-1 rounded-full'>Podcasts</p>
+        <button className="cursor-pointer font-medium">
+          <p className="bg-[#242424] hover:bg-[#3e3e3e] text-gray-400 hover:text-white transition-all duration-300 px-4 py-1 rounded-full">
+            Podcasts
+          </p>
         </button>
       </div>
     </>
   );
-}
+};
 
 export default Navbar;
