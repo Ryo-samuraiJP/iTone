99239ed5f68127a4c545fe1903f30a2ac89e30df
Rio <onlytru6340@gmail.com>
2025-02-24 21:02:49 -0800

    Add comments in MusicPlayer and MobileNavbar components, improving readability and maintainability

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
-import { MdOutlineArrowBack, MdOutlineArrowForward, MdOutlineLibraryMusic, MdOutlineSettings } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
-import { AiFillHome } from 'react-icons/ai';
+import React from "react";
+import {
+  MdOutlineArrowBack,
+  MdOutlineArrowForward,
+  MdOutlineLibraryMusic,
+  MdOutlineSettings,
+} from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
 
+// MobileNavbar component that displays the Music, Back, Home, Forward, and Settings icons
 const MobileNavbar = () => {
   const navigate = useNavigate();
   return (
-    <div className='fixed bottom-4 left-0 right-0 flex justify-around items-center h-10 bg-black text-gray-400 sm:hidden z-40'>
-      <button to='/' className='flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300'>
-        <MdOutlineLibraryMusic className='text-2xl' />
+    <div className="fixed bottom-4 left-0 right-0 flex justify-around items-center h-10 bg-black text-gray-400 sm:hidden z-40">
+      <button
+        to="/"
+        className="flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300"
+      >
+        <MdOutlineLibraryMusic className="text-2xl" />
       </button>
-      <button onClick={() => navigate(-1)} className='flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300'>
-        <MdOutlineArrowBack className='text-2xl' />
+      <button
+        onClick={() => navigate(-1)}
+        className="flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300"
+      >
+        <MdOutlineArrowBack className="text-2xl" />
       </button>
-      <button to='/' className='flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300'>
-        <AiFillHome onClick={() => navigate('/')} className='text-2xl' />
+      <button
+        to="/"
+        className="flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300"
+      >
+        <AiFillHome onClick={() => navigate("/")} className="text-2xl" />
       </button>
-      <button onClick={() => navigate(1)} className='flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300'>
-        <MdOutlineArrowForward className='text-2xl' />
+      <button
+        onClick={() => navigate(1)}
+        className="flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300"
+      >
+        <MdOutlineArrowForward className="text-2xl" />
       </button>
-      <button to='/' className='flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300'>
-        <MdOutlineSettings className='text-2xl' />
+      <button
+        to="/"
+        className="flex flex-col items-center cursor-pointer hover:text-white transition-all duration-300"
+      >
+        <MdOutlineSettings className="text-2xl" />
       </button>
     </div>
   );
-}
+};
 
 export default MobileNavbar;
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,56 +1,97 @@
-import React, { useContext } from 'react';
-import { IoIosShuffle, IoIosSkipBackward, IoIosSkipForward, IoIosPlay, IoIosPause, IoIosInfinite } from "react-icons/io";
+import React, { useContext } from "react";
+import {
+  IoIosShuffle,
+  IoIosSkipBackward,
+  IoIosSkipForward,
+  IoIosPlay,
+  IoIosPause,
+  IoIosInfinite,
+} from "react-icons/io";
 import { AiOutlinePlaySquare } from "react-icons/ai";
 import { TbMicrophone2 } from "react-icons/tb";
 import { HiOutlineQueueList, HiOutlineSpeakerWave } from "react-icons/hi2";
 import { LuMonitorSpeaker } from "react-icons/lu";
 import { CgMiniPlayer } from "react-icons/cg";
 import { RiExpandDiagonalLine } from "react-icons/ri";
-import { PlayerContext } from '../context/PlayerContext';
+import { PlayerContext } from "../context/PlayerContext";
 
+// MusicPlayer component that displays the currently playing song, play/pause buttons, seek bar, etc.
 const MusicPlayer = () => {
-  const { track, seekBg, seekBar, seekPoint, playStatus, play, pause, time, playPrev, playNext } = useContext(PlayerContext);
+  const {
+    track,
+    seekBg,
+    seekBar,
+    seekPoint,
+    playStatus,
+    play,
+    pause,
+    time,
+    playPrev,
+    playNext,
+  } = useContext(PlayerContext);
 
   return (
-    <div className='bottom-7 sm:bottom-0 left-0 right-0 z-50 h-[10%] bg-black flex justify-between items-center text-white pt-3 sm:pt-0 mb-5 sm:mb-0 px-4'>
-      <div className='hidden sm:flex items-center gap-4 w-1/5'>
-        <img className='w-12' src={track.image} alt='currently playing song image' />
-        <div className='truncate w-full'>
-          <p className='truncate'>{track.name}</p>
-          <p className='truncate'>{track.artist}</p>
+    <div className="bottom-7 sm:bottom-0 left-0 right-0 z-50 h-[10%] bg-black flex justify-between items-center text-white pt-3 sm:pt-0 mb-5 sm:mb-0 px-4">
+      <div className="hidden sm:flex items-center gap-4 w-1/5">
+        <img
+          className="w-12"
+          src={track.image}
+          alt="currently playing song image"
+        />
+        <div className="truncate w-full">
+          <p className="truncate">{track.name}</p>
+          <p className="truncate">{track.artist}</p>
         </div>
       </div>
-      <div className='flex flex-col items-center m-auto'>
-        <div className='flex gap-2'>
-          <IoIosShuffle className='text-2xl cursor-pointer' />
-          <IoIosSkipBackward onClick={playPrev} className='text-2xl cursor-pointer' />
-          {playStatus 
-            ? <IoIosPause onClick={pause} className='text-2xl cursor-pointer' />
-            : <IoIosPlay onClick={play} className='text-2xl cursor-pointer' />
-          }
-          <IoIosSkipForward onClick={playNext} className='text-2xl cursor-pointer' />
-          <IoIosInfinite className='text-2xl cursor-pointer' />
+      <div className="flex flex-col items-center m-auto">
+        <div className="flex gap-2">
+          <IoIosShuffle className="text-2xl cursor-pointer" />
+          <IoIosSkipBackward
+            onClick={playPrev}
+            className="text-2xl cursor-pointer"
+          />
+          {playStatus ? (
+            <IoIosPause onClick={pause} className="text-2xl cursor-pointer" />
+          ) : (
+            <IoIosPlay onClick={play} className="text-2xl cursor-pointer" />
+          )}
+          <IoIosSkipForward
+            onClick={playNext}
+            className="text-2xl cursor-pointer"
+          />
+          <IoIosInfinite className="text-2xl cursor-pointer" />
         </div>
-        <div className='flex items-center gap-5'>
-          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
-          <div ref={seekBg} onClick={seekPoint} className='w-[60vw] max-w-[300px] bg-gray-500 rounded-full cursor-pointer'>
-            <hr ref={seekBar} className='h-1 border-none w-0 bg-gray-200 rounded-full' />
+        <div className="flex items-center gap-5">
+          <p>
+            {time.currentTime.minute}:{time.currentTime.second}
+          </p>
+          <div
+            ref={seekBg}
+            onClick={seekPoint}
+            className="w-[60vw] max-w-[300px] bg-gray-500 rounded-full cursor-pointer"
+          >
+            <hr
+              ref={seekBar}
+              className="h-1 border-none w-0 bg-gray-200 rounded-full"
+            />
           </div>
-          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
+          <p>
+            {time.totalTime.minute}:{time.totalTime.second}
+          </p>
         </div>
       </div>
-      <div className='text-lg hidden xl:flex items-center gap-1.5 opacity-75'>
+      <div className="text-lg hidden xl:flex items-center gap-1.5 opacity-75">
         <AiOutlinePlaySquare />
         <TbMicrophone2 />
         <HiOutlineQueueList />
         <LuMonitorSpeaker />
         <HiOutlineSpeakerWave />
-        <div className='w-20 bg-slate-50 h-1 rounded'></div>
+        <div className="w-20 bg-slate-50 h-1 rounded"></div>
         <CgMiniPlayer />
         <RiExpandDiagonalLine />
       </div>
     </div>
   );
-}
+};
 
 export default MusicPlayer;
